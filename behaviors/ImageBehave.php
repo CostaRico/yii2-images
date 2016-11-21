e1f1fff66844afa859b2583e8f76ef83af9e5bb2
andku83 <andreykushnarev83@gmail.com>
2016-11-21 17:19:27 +0200

    Update ImageBehave.php
    If model no image and PlaceHolder is not defined getImages() return: array([0] => NULL).
    But in removeImages() expected count(array()) < 1.
    
    Fix to return: array()

--- behaviors/ImageBehave.php
+++ behaviors/ImageBehave.php
@@ -183,7 +183,7 @@ class ImageBehave extends Behavior
         $imageQuery->orderBy(['isMain' => SORT_DESC, 'id' => SORT_ASC]);
 
         $imageRecords = $imageQuery->all();
-        if(!$imageRecords){
+        if(!$imageRecords && $this->getModule()->placeHolderPath){
             return [$this->getModule()->getPlaceHolder()];
         }
         return $imageRecords;
