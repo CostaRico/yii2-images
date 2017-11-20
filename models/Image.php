3586f09edb70f97f309c21cf80666a96828ba99c
David Baltusavich <david@synatree.com>
2017-11-20 14:23:39 -0500

    replace "id" with getPrimaryKey()

--- models/Image.php
+++ models/Image.php
@@ -52,7 +52,7 @@ class Image extends \yii\db\ActiveRecord
     public function getUrl($size = false){
         $urlSize = ($size) ? '_'.$size : '';
         $url = Url::toRoute([
-            '/'.$this->getModule()->id.'/images/image-by-item-and-alias',
+            '/'.$this->getModule()->getPrimaryKey().'/images/image-by-item-and-alias',
             'item' => $this->modelName.$this->itemId,
             'dirtyAlias' =>  $this->urlAlias.$urlSize.'.'.$this->getExtension()
         ]);
